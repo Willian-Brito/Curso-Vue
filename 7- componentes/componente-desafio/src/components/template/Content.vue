2983471485378b6120392020ba484288349d8628
Willian Brito <willian_brito00@hotmail.com>
2024-05-05 10:37:28 -0300

    Adicionando exercicios de vue cli e componentes

---
+++ 7- componentes/componente-desafio/src/components/template/Content.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-content>
+        <app-carousel />
+    </v-content>
+</template>
+
+<script>
+import Carousel from '../widgets/Carousel.vue'
+
+export default {
+    components: { 'app-carousel': Carousel }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ 7- componentes/componente-desafio/src/main/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <NavBar />    
+    <Menu />
+    <Content />
+    <Footer />    
+  </v-app>
+</template>
+
+<script>
+
+import NavBar from '@/components/template/NavBar.vue'
+import Menu from '@/components/template/Menu.vue'
+import Content from '@/components/template/Content.vue'
+import Footer from '@/components/template/Footer.vue'
+
+export default {
+  components: { NavBar, Menu, Content, Footer  }
+}
+</script>
