d4f4403fc23de1ccef4d2cc35ac39b3842723c88
Willian Brito <willian_brito00@hotmail.com>
2024-06-02 12:09:54 -0300

    exercicios de rotas com o vue-router

---
+++ 16- rotas/rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="usuario-detalhe">
+        <h3>Usuário Detalhe</h3>        
+        <p><strong>Codigo: </strong> {{ id }} / {{ $route.params.id }}</p>
+        <!-- :to="`/usuario/${id}/editar`"> -->
+        <router-link 
+            tag="button" 
+            primario 
+            :to="{ name: 'editarUsuario', params: {id}, query: {completo: false, lingua: 'en'}, hash: '#rodape' }">
+            Editar
+        </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['id'],
+    beforeRouteEnter(to, from, next) {
+        console.log('dentro do componente -> usuario detalhe')
+        const autenticado = true
+
+        autenticado ? next() : next(false)
+    }
+    // data() {
+    //     return {
+    //         id: this.$route.params.id
+    //     }
+    // },
+    // watch: {
+    //     $route(to, from) {
+    //         this.id = to.params.id
+    //     }
+    // },
+}
+</script>
+
+<style>
+
+</style>
